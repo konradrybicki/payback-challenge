610734cc71b498645e082464f8e3267208f82e3f
Konrad Rybicki <konrad.rybicki.dev@gmail.com>
2023-05-23 18:11:05 +0200

    Repository setup

---
+++ PaybackChallenge/PaybackChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaybackChallengeApp.swift
+//  PaybackChallenge
+//
+//  Created by Konrad Rybicki on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PaybackChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
